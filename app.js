ae35566bdabb624d36953b32c834023f754fcf24
vinnik-v <vinnikviv@gmail.com>
2019-07-16 22:29:16 +0300

    last updates

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const path = require ('path');
 const keys = require('./keys');
 const postRouter = require('./routes/post');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 5000; //проверяем существует ли системная переменная порта, если ее нет задаем вручную
 const clientPath = path.join(__dirname, 'client');
@@ -12,6 +13,8 @@ mongoose.connect(keys.mongoURI)
     .catch(err => console.error(err)) //если не успешно
 
 const app = express();
+app.use(bodyParser.json());
+app.use('/api/post', postRouter);
 app.use(express.static(clientPath));
 
 app.listen(port, () => {
